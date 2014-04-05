5f178c226882970f580ecf273a5d038fd23c403e
Chris Tyrrel <chasedspade@chasedspade.com>
2014-04-04 19:22:49 -0600

    Added GPL Boilerplate

--- botnet/commands.tcl
+++ botnet/commands.tcl
@@ -1,3 +1,19 @@
+# SBScripts - A management/botnet script system to help Channel Owners/Admins
+# Copyright (C) 2014 Chris Tyrrel
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 bind pub - !relay pub:relay
 proc pub:relay {nick host hand chan text} {
 	set rel [lrange $text 0 end]
