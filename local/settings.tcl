023a3f291f60d34f0086b7e3c430a738a2dfa25e
Chris Tyrrel <chasedspade@chasedspade.com>
2014-04-13 16:21:25 -0600

    Part 1 of new Script System

---
+++ local/settings.tcl
@@ -0,0 +1,11 @@
+# This channel should not be publically accessible as secure information from the bot is relayed to it.
+set AdminChan "#AdminChannel" 
+
+# The Network Name This Bot Is On
+set network "Network Sending Message"
+
+# Your Bot's NickServ password (if Network has Services)
+set NickServ "NickServ Password"
+
+# Main Channel
+set MainChan "#MainChannel"
