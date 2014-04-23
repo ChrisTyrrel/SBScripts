5f63cb89345eca6a1683322847cc1d0c381f7758
Chris Tyrrel <chasedspade@chasedspade.com>
2014-04-22 23:23:09 -0600

    Net is not spelled "Next"

--- SBScripts.tcl
+++ SBScripts.tcl
@@ -26,7 +26,7 @@ source "$SBScriptsLocal/settings.tcl"
 	putlog "SBScripts settings have been loaded from $SBScriptsLocal."
 source "$SBScriptsModules/administration/commands.tcl"
 	putlog "SBScripts administration commands have been loaded."
-if {$BotNextExist == "1"} {
+if {$BotNetExist == "1"} {
 	source "$SBScriptsModules/botnet/commands.tcl"
 	putlog "SBScripts botnet commands have been loaded."
 	source "$SBScriptsModules/botnet/receivers.tcl"
